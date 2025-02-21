7eda122f1ffd9e711d16c6d541883461e0b5f48d
MyOwlhelp <thatlilguy23@gmail.com>
2025-02-21 21:48:55 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 --[[ Synapse Y
      Date:      2/17/2025
-     Desc: 	Open Sourced.
+     Desc: 		Open Sourced.
      File:      Init.lua
 ]]
 
@@ -205,7 +205,6 @@ getgenv().require = function(scr, Req)
 	return res
 end
 
-
 local function decompile(script_instance)
 	local bytecode = getscriptbytecode(script_instance)
 	local encoded = crypt.base64.encode(bytecode)
@@ -237,6 +236,36 @@ getgenv().saveinstance = newcclosure(function()
 	local Options = { NilInstances = true, RemovePlayerCharacters = false }
 	synsaveinstance(Options)
 end)
+
+local CoreGui = game:GetService("CoreGui")
+
+-- Hook the CoreGui functions
+local mt = getrawmetatable(CoreGui)
+setreadonly(mt, false)
+
+local oldIndex = mt.__index
+
+mt.__index = function(self, key)
+	if self == CoreGui then
+		if key == "ToggleRecording" or 
+			key == "TakeScreenshot" or 
+			key == "SendCommand" or 
+			key == "ExecuteJavaScript" or 
+			key == "GetAsyncFullUrl" then
+			return function(...)
+				warn("Blocked attempt to call CoreGui:" .. key)
+				return nil -- Prevent execution
+			end
+		elseif key == "GetRobuxBalance" then
+			return function(...)
+				return nil -- Hide balance
+			end
+		end
+	end
+	return oldIndex(self, key)
+end
+
+setreadonly(mt, true)
 
 --getloadedmodules fix
 getgenv().getloadedmodules = newcclosure(function()
